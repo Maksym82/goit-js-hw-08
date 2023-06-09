3491c079bf30cb2237f83d01fe6c0fe024618788
Maksym <maksym.gromov82@gmail.com>
2023-06-09 20:18:56 +0300

    создаёт задание 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash.throttle';
+
+const refs = {
+    form: document.querySelector('form.feedback-form'),
+    userEmail: document.querySelector('form input'),
+    userMessage: document.querySelector('form textarea'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+let formData = {
+    email: '',
+    message: '',
+};
 
